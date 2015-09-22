5eb2de7a4b85815706d880c4d556fe1f3b1aaa86
Alexey Bondarenko <alexeybondarenko@me.com>
2015-09-22 23:21:06 +0300

    added bird

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -14,30 +14,152 @@
             };
     })();
 
-    var canvas = document.querySelector('#game');
-    var ctx = canvas.getContext('2d');
-
-    canvas.width = 800;
-    canvas.height = 600;
-
     var lstTime = null;
 
+    var container = document.querySelector('#game');
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+
+    container.appendChild(canvas);
+
+    canvas.width = 600;
+    canvas.height = 400;
+
+    var brickPattern = null;
+
+    // Loading resources
+
+    resources.load([
+        'img/brickwall.png'
+    ]);
+    resources.onReady(init);
+
+    // Initializing
+    function init () {
+        console.log('init');
+
+        // backgrounds
+        brickPattern = ctx.createPattern(resources.get('img/brickwall.png'), 'repeat');
+
+        reset();
+        lstTime = Date.now();
+        main();
+    }
+
+    // Main loop
     function main () {
         var now = Date.now();
-        var dt = (now - lasTime) / 1000.0;
+        var dt = (now - lstTime) / 1000.0;
 
         update(dt);
-        render();
+        render(ctx);
 
         lstTime = now;
         requestAnimFrame(main);
     }
+    function reset () {
 
-    function render () {
+    }
 
+    /**
+     * Game loops
+     */
+
+    function render (ctx) {
+        ctx.fillStyle = brickPattern;
+        ctx.fillRect(0, 0, canvas.width,  canvas.height);
+
+        player.render(ctx);
     }
     function update (dt) {
+        handleInputs (dt);
 
+        player.update(dt);
+
+        checkCollisions();
     }
+
+
+    /**
+     * Inputs
+     */
+
+    var keyPressed = {};
+    document.addEventListener('keydown', function (e, data) {
+        keyPressed[e.keyCode] = true;
+    });
+    document.addEventListener('keyup', function (e, data) {
+        keyPressed[e.keyCode] = false;
+    });
+    document.addEventListener('blur', function (e, data) {
+        keyPressed = {};
+    });
+
+    function handleInputs (dt) {
+        if (keyPressed[32]) { // space pressed
+            player.speedUp(dt);
+        }
+    }
+
+    function isHaveCollisions () {
+        if (player.y <= 0 || player.y >= canvas.height) {
+            return true;
+        }
+        return false;
+    }
+    function checkCollisions () {
+        //if (isHaveCollisions()) {
+        //    console.log('have collisions');
+        //}
+    }
+
+    var Tube = function () {
+
+    };
+    Tube.prototype.x = 0;
+    Tube.prototype.width = 20;
+    Tube.prototype.aperture = {
+        width: 20,
+        position: 20 // y position of aperture center in percentage
+    };
+    //Tube.prototype.render = function (ctx) {
+    //    ctx.fillStyle =
+    //}
+    /**
+     * Birst
+     */
+    var Bird = function () {
+
+    };
+    Bird.prototype.acceleration = -9.8;
+
+    Bird.prototype.speed = 0;
+    Bird.prototype.x = 200;
+    Bird.prototype.y = 50;
+
+    // render bird on canvas
+    Bird.prototype.render = function (ctx) {
+        ctx.fillStyle = "#00A308";
+        ctx.beginPath();
+        ctx.arc(this.x, canvas.height - this.y, 10, 0, Math.PI*2, true);
+        ctx.closePath();
+        ctx.fill();
+    };
+    // update object based on the time delta
+    Bird.prototype.update = function (dt) {
+        //dt *= 10;
+        this.speed += this.acceleration * dt * 90;
+        this.y += this.speed * dt;
+        if (this.y < 0) {
+            this.y = 0;
+            this.speed = 0;
+        }
+    };
+    Bird.prototype.speedUp = function (x, y) {
+        this.speed += 80;
+    };
+
+    var player = new Bird ();
+
 
 })();
